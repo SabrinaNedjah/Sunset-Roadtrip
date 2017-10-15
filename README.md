9ad3b7f4c2191c7999a9ffefcb2b76686d38ac1c
Sabrina Nedjah <snedjah78@gmail.com>
2017-10-15 23:54:08 +0200

    final patch

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Sunset RoadTrip
-Ride accross the desert with a 4x4.
+Ride across the desert with a 4x4.
 
 ### Setup
 ```bash
@@ -7,12 +7,6 @@ git clone git@github.com:SabrinaNedjah/Sunset-Roadtrip.git
 ```
 
 Then open the `index.html` file in the `/src` folder.
-
-**IMPORTANT :**
-Main.js contains const, arrow functions or deconstructed parameters, It's ES6 only.
-Without Babel, it doesn't work everywhere
-
-* [Babeljs](https://babeljs.io/)
 
 ### Features
 - Day / Night mode (touch the sky to enable/disabled)
--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -1,43 +1,44 @@
-// IMPORTANT :
-// WORK WITH MODERN NAVIGATOR (chrome etc...) 
-// const, arrow functions or deconstructed parameters, It's ES6 only.
-// Without Babel, it doesn't work everywhere
-
 
 ////////////
 //SETTING //
 ///////////
 
-const sky = document.querySelector('.sky');
-const sun = document.querySelector('.sun');
-const moon = document.querySelector('.moon');
+const pinkSky = document.querySelector('.pinkSky');
+const sunShine = document.querySelector('.sunShine');
+const moonLight = document.querySelector('.moonLight');
 
-const activate = (...elems) => 
-  elems.forEach(elem =>
-    elem.classList.add('active'))
-
-const deactivate = (...elems) =>
-  elems.forEach(elem =>
-    elem.classList.remove('active'))
-
-const random = (scale, shift = 0) =>
-  Math.floor(Math.random() * scale + shift)
-
-let nightTime = false;
-let nextMeteor = 0;
-let meteorTimer;
-
-let randomTimer = () => {
-  newMeteor();
-  meteorTimer = setTimeout(() => {
-    randomTimer();
-    nextMeteor = random(5000, 1000);
-  }, nextMeteor)
+const activate = function (pinkSky, sunShine, moonLight) {
+  pinkSky.classList.add('active');
+  sunShine.classList.add('active');
+  moonLight.classList.add('active');
 }
 
-let stopTimer = () => {
+const desactivate = function (pinkSky, sunShine, moonLight) {
+  pinkSky.classList.remove('active');
+  sunShine.classList.remove('active');
+  moonLight.classList.remove('active');
+}
+
+const random = function (scale, shift = 0) {
+  return Math.floor(Math.random() * scale + shift)
+}
+
+let timeNight = false;
+let meteorAfter = 0;
+let meteorTimer;
+
+
+let randTimer = function () {
+  newMeteor();
+  meteorTimer = setTimeout(function (){
+    randTimer();
+    meteorAfter = random(5000, 1000);
+  }, meteorAfter)
+}
+
+let stopTimer = function () {
   clearInterval(meteorTimer);
-  nextMeteor = 6000;
+  meteorAfter = 6000;
 }
 
 ////////////
@@ -51,68 +52,67 @@ for(let i = 0; i < 512; i++) {
   star.classList.add('star');
   star.style.left = posX + 'vw';
   star.style.bottom = posY + 'vh';
-  sky.appendChild(star); 
+  pinkSky.appendChild(star); 
 }
 
-let stars = sky.querySelectorAll('.star');
+let stars = pinkSky.querySelectorAll('.star');
 
 ////////////
 // METEOR //
 // SHOWER //
 ///////////
 
-const newMeteor = () => {
-  console.log(nextMeteor);
-  let posX = random(100);
-  let posY = random(75, 25);
-  let transX = random(40, 10);
-  let transY = random(15, 15);
-  let opacity = Math.random();
-  let bias = transY / 100;
-  let meteor = document.createElement('div');
+const newMeteor = function () {
+  const posX = random(100);
+  const posY = random(75, 25);
+  const transX = random(40, 10);
+  const transY = random(15, 15);
+  const opacity = Math.random();
+  const bias = transY / 100;
+  const meteor = document.createElement('div');
 
   meteor.classList.add('meteor');
   meteor.style.left = `${posX}vw`;
   meteor.style.bottom = `${posY}vh`;
 
-  meteor.addEventListener('transitionend', () => {
-    sky.removeChild(meteor);
+  meteor.addEventListener('transitionend', function () {
+    pinkSky.removeChild(meteor);
   });
 
-  sky.appendChild(meteor);
-  setTimeout(() => {
+  pinkSky.appendChild(meteor);
+  setTimeout(function () {
     meteor.style.opacity = 0;
     meteor.style.transform =
       `translateX(${transX}vw) translateY(${transY}vh)`;
   }, 15)  
 }
 
-const time = () => {  
-  if (nightTime) {
-    setTimeout(randomTimer, 5500)
-    activate(sky, sun, moon);
+const time = function () {  
+  if (timeNight) {
+    setTimeout(randTimer, 5500)
+    activate(pinkSky, sunShine, moonLight);
     stars.forEach(star => {
-      let opacity = Math.random();
-      let getPosY = 
+      const opacity = Math.random();
+      const getPosY = 
           parseInt(
             star.style.bottom
             .slice(0, -2), 10
           );
       //stars less opaque
-      let bias = getPosY / 100
+      const bias = getPosY / 100
       star.style.opacity = opacity * bias;
     })  
   } else {
     stopTimer();
-    deactivate(sky, sun, moon);
+    desactivate(pinkSky, sunShine, moonLight);
     stars.forEach(star => {
       star.style.opacity = 0;
     })
   } 
 }
 
-sky.addEventListener('click', () => {
-  nightTime = !nightTime;
+pinkSky.addEventListener('click', function () {
+  timeNight = !timeNight;
   time();
 })
 
